6a66cecdc71d37d3be219a88ce98415cc0fe695e
lan hao <lanhao@catarc.ac.cn>
2022-08-18 20:11:11 +0800

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   boards/__init__.py
    	new file:   boards/__pycache__/__init__.cpython-38.pyc
    	new file:   boards/__pycache__/admin.cpython-38.pyc
    	new file:   boards/__pycache__/apps.cpython-38.pyc
    	new file:   boards/__pycache__/models.cpython-38.pyc
    	new file:   boards/__pycache__/tests.cpython-38.pyc
    	new file:   boards/__pycache__/urls.cpython-38.pyc
    	new file:   boards/__pycache__/views.cpython-38.pyc
    	new file:   boards/admin.py
    	new file:   boards/apps.py
    	new file:   boards/migrations/0001_initial.py
    	new file:   boards/migrations/__init__.py
    	new file:   boards/migrations/__pycache__/0001_initial.cpython-38.pyc
    	new file:   boards/migrations/__pycache__/__init__.cpython-38.pyc
    	new file:   boards/models.py
    	new file:   boards/static/css/bootstrap.min.css
    	new file:   boards/templates/home.html
    	new file:   boards/tests.py
    	new file:   boards/urls.py
    	new file:   boards/views.py
    	new file:   db.sqlite3
    	new file:   manage.py
    	new file:   myproject/__init__.py
    	new file:   myproject/__pycache__/__init__.cpython-38.pyc
    	new file:   myproject/__pycache__/settings.cpython-38.pyc
    	new file:   myproject/__pycache__/urls.cpython-38.pyc
    	new file:   myproject/__pycache__/wsgi.cpython-38.pyc
    	new file:   myproject/asgi.py
    	new file:   myproject/settings.py
    	new file:   myproject/urls.py
    	new file:   myproject/wsgi.py

---
+++ boards/views.py
@@ -0,0 +1,15 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Board
+
+def home(request):
+        boards = Board.objects.all()
+        # boards_names = list()
+        # for board in boards:
+        #         boards_names.append(board.name)        
+        # boards_names =[board.name for board in boards]
+        # response_html = '<br>'.join(boards_names)
+        # return HttpResponse (response_html)
+        return render(request,'home.html',{'boards':boards})
+# Create your views here.
