aa71f02a9feacff1c7cd996936e4e853c955c180
lan hao <lanhao@catarc.ac.cn>
2022-08-19 10:38:31 +0800

    Changes to be committed: 	modified:   myproject/__pycache__/settings.cpython-38.pyc 	modified:   myproject/settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -78,13 +78,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': dj_database_url.config(
-    default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': dj_database_url.config(
+    # default=config('DATABASE_URL')
+    # )
 }
 
 
